135ebb1ed9b601b4541b8230f165031994650d9e
AllieDoan <rosendeeno@gmail.com>
2022-05-24 23:03:11 -0500

    init

---
+++ main.js
@@ -0,0 +1,43 @@
+const button = document.getElementById('btn');
+const joke = document.getElementById('joke');
+
+console.log(joke);
+
+async function generateRandomJoke() {
+    const response = await fetch('https://icanhazdadjoke.com/', {
+        headers: {
+            'Accept': 'application/json'
+        }
+    });
+    const data = await response.json();
+
+    console.log(data);
+
+    joke.innerText = data.joke;
+}
+
+// function generateRandomJoke() {
+//     fetch('https://icanhazdadjoke.com/', {
+//         headers: {
+//             'Accept': 'application/json'
+//         }
+//     })
+//         .then(response => {
+//             return response.json();
+//         })
+//         .then(data => {
+//             joke.innerText = data.joke;
+//         })
+// }
+
+// async function generateRandomJoke() {
+//     const response = await fetch('https://api.chucknorris.io/jokes/random');
+//     const data = await response.json();
+
+//     console.log(data);
+
+//     joke.innerText = data.value;
+// }
+
+
+button.addEventListener('click', generateRandomJoke);
